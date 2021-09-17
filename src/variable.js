bb95ce5bb4cd9da3afcdd6af6920dd84a83089eb
carlosmendezs <carlosmen1987@gmail.com>
2021-09-17 16:11:52 -0700

    actualzación codigo login y creación página principal

--- src/variable.js
+++ src/variable.js
@@ -1,30 +1,14 @@
-var nueva = "Hola" //var variable constante
-console.log(nueva) //carga la variable y la imprime
-/*
-let nueva1 = "Hola Mundo" //let variable
-
-document.write(nueva) //la escribe y la muestra en el navegador
-*/
-
-if (true){
-    let nueva = "Hola Mundo"
-    console.log(nueva)
-}
-console.log(nueva)
-const server = "192.168.1.1"
-console.log(server)
-
-let numero = 1.5;
-let dogs = "puppy";
-let boll = true;
-
-
-// crear funciones
-function fnueva(num1, num2, stade = false){
-    result = num1*num2
-    if (stade == false){
-        console.log(result)
+function obtenerdatos(){
+    var usuario = document.getElementById('user').value;
+    var contraseña = document.getElementById('password').value;
+    
+    if (usuario=="admin" && contraseña=="principal"){
+        alert("Bienvenido " + usuario);
+        setTimeout(window.open("principal.html"), 2000);
+        window.close();
+        return false;
     }else{
-        alert(result)
+        alert("Usuario o contraseña incorrectos")  
     }
+
 }
