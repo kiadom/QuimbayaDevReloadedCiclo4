57c1ce91e888d81b3885114b2d5b24022b8e8a2f
JuanMa <ingjuanmasuarez@gmail.com>
2021-09-27 07:01:30 -0500

    mejora del esquema de react, aun falta realizar el redireccionamiento

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,35 @@
+import '../styles/style2.css';
+
 function index_page(){
-    return <div>Esta es la pagina de index</div>
+    return <div>
+               
+                <div class="container overflow-hidden" id="grid">
+                <div class="row gy-5">
+                    <div class="col-6">  
+                    <div class="p-3 bg-light">
+                        <a href="admin_ventas.html">  <img src="images/gestion-ventas.png" alt="" width="100" height="100"/></a>
+                        Gestion de Ventas</div>
+                    </div>
+                    
+                    <div class="col-6">
+                    <div class="p-3 bg-light">
+                        <a href="registro_estado_ventas.html"><img src="images/estado-ventas.png" alt="" width="100" height="100"/></a>
+                        Estado de Ventas</div>
+                    </div>
+                    
+                    <div class="col-6">
+                    <div class="p-3 bg-light">
+                        <a href="admin_usuarios.html"><img src="images/gestion-usuario.png" alt="" width="100" height="100"/></a>
+                        Gestion de Usuarios</div>
+                    </div>
+                    <div class="col-6">
+                    <div class="p-3 bg-light">
+                        <a href="admin_vendedores.html"><img src="images/gestion-informacion.png" alt="" width="100" height="100"/></a>
+                        Gestion de Información</div>
+                    </div>
+                </div>
+                </div>
+            </div>   
 }
 
 export default index_page;
