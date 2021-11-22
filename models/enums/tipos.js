b94f7d8ff099cab0d7949183e7e82e3e27aec3fe
nacuervo <nelsoncuervo86@gmail.com>
2021-11-22 06:55:44 -0500

    agregar enums para estado usuario y rol

--- models/enums/tipos.js
+++ models/enums/tipos.js
@@ -2,6 +2,18 @@ import { gql } from 'apollo-server-express';
 
 const tiposEnums = gql`
 
+enum  Enum_EstadoUsuario{
+        PENDIENTE
+        AUTORIZADO
+        NO_AUTORIZADO
+    }
+
+    enum Enum_Rol{
+        ESTUDIANTE
+        LIDER
+        ADMINISTRADOR
+    }
+    
     enum Enum_EstadoProyecto{
         ACTIVO
         INACTIVO
