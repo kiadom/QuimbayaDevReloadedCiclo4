e18a1535930554415cf955a190bcd4ac9eec2ba2
nacuervo <nelsoncuervo86@gmail.com>
2021-11-22 22:51:20 -0500

    Terminar modelo usuario, tipos y resolvers, cambio de nombre archivo types.js en la carpeta graphql por tipos.js

---
+++ models/usuario/resolvers.js
@@ -0,0 +1,59 @@
+import { ModeloUsuario } from "./usuario.js"
+
+const resolversUsuario = {
+    Query:{
+        Usuarios: async (parent, args) => {
+            const usuarios = await ModeloUsuario.find();
+            return usuarios;
+        },
+
+        Usuario: async (parent, args) => {
+            const usuario = await ModeloUsuario.findOne({
+                _id: args._id
+            });
+            return usuario;
+        },
+    },
+
+    Mutation:{
+        crearUsuario: async (parent, args) => {
+            const usuarioCreado = await ModeloUsuario.create({
+                correo:args.correo,
+                identificacion:args.identificacion,
+                nombre:args.nombre,
+                apellido:args.apellido,
+                contrasena:args.contrasena,
+                rol:args.rol
+            });
+            return usuarioCreado;
+        },
+
+        editarUsuario: async (parent, args) => {
+            const usuarioEditado = await ModeloUsuario.findOneAndUpdate(args._id,{
+                correo:args.correo,
+                identificacion:args.identificacion,
+                nombre:args.nombre,
+                apellido:args.apellido,
+                contrasena: args.contrasena,
+                rol:args.rol,
+                estado: args.estado
+            });
+            return usuarioEditado;
+        },
+
+        eliminarUsuario: async (paent, args) => {
+            if(Object.keys(args).includes('_id')){
+                const usuarioEliminado = await ModeloUsuario.findOneAndDelete({_id:args._id});
+                return usuarioEliminado;
+            }else if(Object.keys(args).includes('correo')){
+                const usuarioEliminado = await ModeloUsuario.findOneAndDelete({correo:args.correo});
+                return usuarioEliminado;
+            }else if(Object.keys(args).includes('identificacion')){
+                const usuarioEliminado = await ModeloUsuario.findOneAndDelete({identificacion:args.identificacion});
+                return usuarioEliminado;
+            }
+        },
+    },
+};
+
+export {resolversUsuario}
---
+++ models/usuario/tipos.js
@@ -0,0 +1,54 @@
+import { gql } from 'apollo-server-express';
+
+const tiposUsuario = gql`
+
+    type Usuario {
+        _id: ID!
+        correo: String!
+        identificacion: String!
+        nombre: String!
+        apellido: String!
+        contrasena: String!
+        rol: Enum_Rol!
+        estado: Enum_EstadoUsuario
+        """ projects:[Project]
+        advances:[Advance]
+        inscriptions:[Inscription] """
+    }
+
+    type Query {
+        Usuarios: [Usuario]
+        Usuario(_id:String!): Usuario
+    }
+
+    type Mutation {
+        crearUsuario(
+            correo: String!
+            identificacion: String!
+            nombre: String!
+            apellido: String!
+            contrasena: String!
+            rol: Enum_Rol!
+            estado: Enum_EstadoUsuario
+        ):Usuario
+
+        editarUsuario(
+            _id: String!
+            correo: String!
+            identificacion: String!
+            nombre: String!
+            apellido: String!
+            contrasena: String!
+            rol: Enum_Rol!
+            estado: Enum_EstadoUsuario
+        ): Usuario
+
+        eliminarUsuario(
+            _id:String
+            correo:String
+            identificacion:String
+        ):Usuario
+    }
+`;
+
+export { tiposUsuario };
