1921867d8d843ea1db8979fee63b5c75a54832bd
JuanMa <ingjuanmasuarez@gmail.com>
2021-11-24 07:23:44 -0500

    cambio a español de proyectos y objetivos

--- graphql/tipos.js
+++ graphql/tipos.js
@@ -1,8 +1,8 @@
 import { gql } from 'apollo-server-express';
 import { tiposEnums } from '../models/enums/tipos.js';
 import { tiposUsuario } from '../models/usuario/tipos.js';
-import { tiposProyecto } from '../models/project/tipos.js';
-import { tiposObjetivo } from '../models/objective/tipos.js';
+import { tiposProyecto } from '../models/proyecto/tipos.js';
+import { tiposObjetivo } from '../models/objetivo/tipos.js';
 import { tiposInscripcion } from '../models/inscription/tipos.js';
 
 const tiposGlobales = gql`
