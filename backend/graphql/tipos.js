00086696110ea1f21f6c026f612b5ce1464379fc
Rocio Pacheco <pacheco.rocio@gmail.com>
2021-12-04 17:14:12 -0500

    Lo quesea

--- backend/graphql/tipos.js
+++ backend/graphql/tipos.js
@@ -11,8 +11,4 @@ const tiposGlobales = gql`
 `;
 
 
-<<<<<<< HEAD:graphql/tipos.js
 export const tipos = [tiposGlobales, tiposEnums, tiposUsuario, tiposProyecto, tiposObjetivo, tiposInscripcion, tiposAvance]
-=======
-export const tipos = [tiposGlobales, tiposEnums, tiposUsuario, tiposProyecto, tiposObjetivo, tiposInscripcion, tiposAvance]
->>>>>>> c29da4692f5e514f1a5c97fed2f9dfb4700975bd:backend/graphql/tipos.js
