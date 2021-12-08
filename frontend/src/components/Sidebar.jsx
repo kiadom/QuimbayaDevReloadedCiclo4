7759c1c0de8cdc8ea1779237b9ce45bb5a611602
kiadom <kiadom777@gmail.com>
2021-12-07 22:51:55 -0500

    iconos

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import imagen from '../media/logoheader.png';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-
-import {faAddressCard} from "@fortawesome/free-solid-svg-icons";
+import {faAddressCard, faHome, faUsers, faProjectDiagram} from "@fortawesome/free-solid-svg-icons";
 library.add(faAddressCard);
 function Sidebar(){
     return (
         <div className="wrapper">
             <div className ="sidebar">
                 <div className ="EncabezadoModulo">
-                    <FontAwesomeIcon icon={faAddressCard} size="5x" color='#092133'/>
-                    <h4>Gestión Usuarios</h4>
+                    <FontAwesomeIcon icon={ faProjectDiagram } size="5x" color='#092133'/>
+                    <h4>Gestión Proyectos</h4>
                     <ul>
                         <li>
-                            
+                        {/* <Link to ='/GestionProyectos'> */}
+                            <FontAwesomeIcon icon={faHome} size='1x' color='#092133'/>
+                            <span className=''></span> Inicio
+                        {/* </Link> */}
+                        </li>
+                        <li>
+                        {/* <Link to ='/GestionProyectos'> */}
+                            <FontAwesomeIcon icon={faUsers} size='1x' color='#092133'/>
+                            <span className=''></span> Inscripciones
+                        {/* </Link> */}
                         </li>
                     </ul>
                 </div>
