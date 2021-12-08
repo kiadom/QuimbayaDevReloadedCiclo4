e2efcadadebdf8fed02410c7732ded57946066ce
Rocio Pacheco <pacheco.rocio@gmail.com>
2021-12-08 00:34:49 -0500

    Avances interfaz reportes de avances del proyecto

--- frontend/src/pages/Avances/AvancesPorProyecto.jsx
+++ frontend/src/pages/Avances/AvancesPorProyecto.jsx
@@ -3,10 +3,12 @@ import { useQuery, useMutation } from '@apollo/client';
 import { GET_AVANCESPORPROYECTO } from "../../graphql/avances/querys";
 import { useParams, Link } from "react-router-dom";
 
-
-
-
 const AvancesPorProyecto = () => {
+
+    /* ESTADOS QUE PERMITEN CONTROLAR LA VISIBILIDAD DE LAS INTERFACES */
+    const [textoBoton, setTextoBoton] = useState('Ver Avances Reportados' );
+    const [mostrarTabla, setMostrarTabla] = useState(true);
+
     const { proyecto } = useParams();
     const { data } = useQuery(GET_AVANCESPORPROYECTO,{
         variables:{ proyecto }
@@ -16,18 +18,32 @@ const AvancesPorProyecto = () => {
         console.log("Datos obtenidos con GET_AVANCESPORPROYECTO", data);
     }, [data]);
 
+    /* SE DEFINE EL TEXTO DEL BOTON, INICIALMENTE SERÁ "Registrar Avance" Y MOSTRARÁ LA INTERFAZ DE TABLA*/
+    useEffect(()=>{
+        if (mostrarTabla) {
+            setTextoBoton('Registrar Avance');
+        }
+        else {
+            setTextoBoton('Ver Avances Reportados');
+        }
+    },[mostrarTabla]);
 
     return (
         <div className="body-text">
-            <TablaAvances listaAvanc = { data }/>
+            <button onClick = {() => {
+                setMostrarTabla (!mostrarTabla);
+                }}
+            >{ textoBoton }</button>
+            { mostrarTabla ? (<TablaAvances listaAvances = { data }/>) : (<FormularioRegistroAvances />)}
+
         </div>
     );
 };
 
-const TablaAvances = ({ listaAvanc }) => {
+const TablaAvances = ({ listaAvances }) => {
     return (
         <div>
-            <h1>Lista de Proyectos en los que se Encuentra Inscrito</h1>
+            <h1>Lista de Reportes de Avance del Proyecto</h1>
                 
                 <table>
                     <thead>
@@ -40,22 +56,61 @@ const TablaAvances = ({ listaAvanc }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        <p>Editar Avance</p>
-                            {/*
-                            { listaAvanc &&
-                                listaAvanc.Avances.map((p) => {
-                                    return (
-                                        <p>
-                                            ensayo1
-                                        </p>
-                                    )
-                                }
-                                )}
-                            */}
+                        { listaAvances && 
+                        listaAvances.AvancesPorProyecto.map((p) => {
+                            return (
+                                <tr key = { p.proyecto }>
+                                    <td>{ p.fecha }</td>
+                                    <td>{ p.descripcion }</td>
+                                    <td>{ p.observacionesLider }</td>
+                                    <td>
+                                        <Link to = {`/avances/AvancesPorProyecto/${p._id}` }>
+                                            {/*<FontAwesomeIcon icon={faPencilAlt}/>*/}
+                                            Detalles Avance
+                                        </Link> 
+                                    </td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
         </div>
     )
 }
+
+/* FUNCION QUE CONTIENE LA INTERFAZ DONDE SE ENCUENTRA EL FORMULARIO PARA REGISTRAR LOS PROYECTOS */
+const FormularioRegistroAvances = ()=> {
+    return (
+        <div>
+            <h1>Ingrese el Proyecto</h1>
+            <form>
+                <table>
+                    <tr>
+                        <td>
+                            <p>Nombre del Proyecto: </p>
+                        </td>
+                        <td>
+                            <input type = "text" required/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>Descripcion: </p>
+                        </td>
+                        <td>
+                            <input type = "text" required/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type = "button" value = "Registrar Avance" />
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    )
+}
+
 
 export {AvancesPorProyecto} ;
