c70fc72a9ecbc0e832474d43900bf203e15d5953
smayav <90738852+smayav@users.noreply.github.com>
2021-12-09 13:31:01 -0500

    cambios inscripciones front

--- frontend/src/pages/GestionInscripciones.jsx
+++ frontend/src/pages/GestionInscripciones.jsx
@@ -1,71 +1,132 @@
-import React, {useEffect} from 'react';
-import {  useQuery } from "@apollo/client";
+import React, { useEffect } from 'react';
+import { useMutation, useQuery } from '@apollo/client';
+//import PrivateRoute from 'components/PrivateRoute';
 import {GET_INSCRIPCIONES} from "../graphql/inscripciones/queries";
+import {APROBAR_INSCRIPCION} from '../graphql/inscripciones/mutations';
+import {RECHAZAR_INSCRIPCION} from '../graphql/inscripciones/mutations';
+import  ButtonLoading from '../components/ButtonLoading';
 import { toast } from 'react-toastify';
+import {
+  AccordionStyled,
+  AccordionSummaryStyled,
+  AccordionDetailsStyled,
+} from '../components/Accordion';
 
-function GestionInscripciones () {
+const GestionInscripciones  = () => {
+  const { data, loading, error, refetch } = useQuery(GET_INSCRIPCIONES);
 
-    const { data, error, loading } = useQuery(GET_INSCRIPCIONES);
-
-    useEffect(() => {
-        if (error) {
-          toast.error('Error consultando las inscripciones');
-        }
-      }, [error]);
-    
-      if (loading) return <div className ='cargando'>Cargando....</div>;
-
-
-    return (
-
-        <div className="body-text">
-            <h1>Inscripciones</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>_id</th>
-                        <th>Proyecto</th>
-                        <th>Lider del proyecto</th>
-                        <th>Estudiante Inscrito</th>
-                        <th>Estado</th>
-                        <th>Fecha de ingreso</th>
-                        <th>Fecha de egreso</th>
-                        <th>Editar</th>
-                        
-                    </tr>
-                </thead>
-                <tbody>
-                    {data && 
-                        data.Inscripciones.map((i) => {
-                            return (
-                                <tr key = { i._id }>
-                                    <td>{ i._id}</td>
-                                    <td>{i.proyecto.nombre}</td>
-                                    <td>{ (i.proyecto.lider.nombre)+' '+(i.proyecto.lider.apellido)}</td>
-                                    <td>{ (i.estudianteInscrito.nombre)+' '+(i.estudianteInscrito.apellido)}</td>
-                                    <td>{ i.estadoInscripcion }</td>
-                                    <td>{ i.fecha_ingreso }</td>
-                                    <td>{ i.fecha_egreso }</td>
-                                    <td><button disabled={i.estadoInscripcion !== 'PENDIENTE'} 
-                                    
-                                      >Aprobar</button>
-                                      <button disabled={i.estadoInscripcion !== 'PENDIENTE'} 
-                                    
-                                    >Rechazar</button>
-                                         
-                                    
-                                   </td>
-                                </tr>
-                            )
-                        })}
-                </tbody>
-            </table>
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+  if (loading) return <div>Cargando...</div>;
+  return (
+    <div >
+      <div >
+      <h1>Inscripciones</h1>
+        <div >
+          <AccordionInscripcion
+            titulo='Inscripciones aprobadas'
+            data={data.Inscripciones.filter((el) => el.estadoInscripcion === 'ACEPTADA')}
+          />
+          <AccordionInscripcion
+            titulo='Inscripciones pendientes'
+            data={data.Inscripciones.filter((el) => el.estadoInscripcion === 'PENDIENTE')}
+            refetch={refetch}
+          />
+          <AccordionInscripcion
+            titulo='Inscripciones rechazadas'
+            data={data.Inscripciones.filter((el) => el.estadoInscripcion === 'RECHAZADA')}
+          />
         </div>
-    );
-
+      </div>
+    </div>
+  );
 };
 
+const AccordionInscripcion = ({ data, titulo, refetch = () => {} }) => {
+  return (
+    <AccordionStyled>
+      <AccordionSummaryStyled>
+        {titulo} ({data.length})
+      </AccordionSummaryStyled>
+      <AccordionDetailsStyled>
+        <div className='flex'>
+          {data &&
+            data.map((inscripcion) => {
+              return <Inscripcion inscripcion={inscripcion} refetch={refetch} />;
+            })}
+        </div>
+      </AccordionDetailsStyled>
+    </AccordionStyled>
+  );
+};
 
+const Inscripcion = ({ inscripcion, refetch }) => {
+  const [aprobarInscripcion, { data, loading, error }] = useMutation(APROBAR_INSCRIPCION);
+  const [rechazarInscripcion, { data:dataRechazar, loading: loadingRechazar, error: errorRechazar }] = useMutation(RECHAZAR_INSCRIPCION);
 
+  useEffect(() => {
+    if (data) {
+      toast.success('Modificado con exito');
+      refetch();
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error('Error modificando la inscripcion');
+    }
+  }, [error]);
+
+  const AInscripcion = () => {
+    aprobarInscripcion({
+      variables: {
+        aprobarInscripcionId: inscripcion._id,
+      },
+    });
+  };
+
+  const RInscripcion = () => {
+    rechazarInscripcion({
+      variables: {
+        rechazarInscripcionId: inscripcion._id,
+      },
+    });
+  };
+
+  return (
+    <div className='bg-gray-900 text-gray-50 flex flex-col p-6 m-2 rounded-lg shadow-xl'>
+
+        
+      <span>{inscripcion.proyecto.nombre}</span>
+      <span>{inscripcion.estudianteInscrito.nombre}</span>
+      <span>{inscripcion.estadoInscripcion}</span>
+      <div>
+      {inscripcion.estadoInscripcion === 'PENDIENTE' && (
+        <ButtonLoading
+          onClick={() => {
+            AInscripcion();
+          }}
+          text='Aprobar Inscripcion'
+          loading={loading}
+          disabled={false}
+        />
+        )}
+        </div>
+        <div>
+        {inscripcion.estadoInscripcion === 'PENDIENTE' && (
+        <ButtonLoading
+          onClick={() => {
+            RInscripcion();
+          }}
+          text='Rechazar Inscripcion'
+          loading={loading}
+          disabled={false}
+        />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export { GestionInscripciones};
