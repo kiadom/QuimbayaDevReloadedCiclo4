bcbe5f52f0b780673bb8ec2dceb8b1b217d5924e
Rocio Pacheco <pacheco.rocio@gmail.com>
2021-12-08 21:54:36 -0500

    enviar avances a gitHub

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 import { ModulesLayout } from './layouts/ModulesLayout';
-<<<<<<< HEAD
-import { DetalleAvances } from "./pages/Avances/DetalleAvances";
-=======
 import { AuthLayout } from './layouts/AuthLayout';
-import { GestionAvances } from './pages/GestionAvances';
->>>>>>> cda2a8f8163ee2745ca7f6d95b6434dfe5a4466f
 import { GestionInscripciones } from './pages/GestionInscripciones';
 import { GestionProyectos } from './pages/GestionProyectos';
 import { GestionProyectosEditar } from './pages/GestionProyectosEditar';
@@ -18,7 +13,10 @@ import Login from './pages/auth/Login';
 import { AuthContext } from "./context/authContext";
 
 import { IndexAvances } from "./pages/Avances/IndexAvances";
-import { AvancesPorProyecto} from "./pages/Avances/AvancesPorProyecto";
+import { AvancesPorProyecto } from "./pages/Avances/AvancesPorProyecto";
+import { DetalleAvances } from "./pages/Avances/DetalleAvances";
+
+//import { GestionAvances } from './pages/GestionAvances';
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -50,17 +48,10 @@ function App() {
             <Routes>
               <Route exact path="/GestionInscripciones" element={<GestionInscripciones/>}/>
               <Route exact path="/GestionProyectos" element={<GestionProyectos/>}/>
-<<<<<<< HEAD
               <Route exact path="/GestionUsuarios" element={<GestionUsuarios/>}/>
               <Route exact path="/Avances/IndexAvances" element={<IndexAvances/>}/>
               <Route exact path="/Avances/AvancesPorProyecto/:proyecto" element={<AvancesPorProyecto/>}/>
               <Route exact path="/Avances/DetalleAvances/:_id" element={<DetalleAvances/>}/>
-=======
-              <Route exact path="/GestionProyectos/Editar/:_id" element={<GestionProyectosEditar />}/>
-              <Route exact path="/GestionUsuarios" element={<GestionUsuarios />}/>         
-              <Route exact path="/auth/Registro" element={<Registro />} />
-              <Route exact path="/auth/Login" element={<Login />} />     
->>>>>>> cda2a8f8163ee2745ca7f6d95b6434dfe5a4466f
             </Routes>
           </ModulesLayout>
 
