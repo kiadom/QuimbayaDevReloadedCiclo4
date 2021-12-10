6cb045e564ef811c81b19ef03b0f4db70ca63627
smayav <90738852+smayav@users.noreply.github.com>
2021-12-09 20:04:42 -0500

    cambios accordion

--- frontend/src/components/Accordion.jsx
+++ frontend/src/components/Accordion.jsx
@@ -4,13 +4,33 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import { styled } from '@mui/material/styles';
 
 const AccordionStyled = styled((props) => <Accordion {...props} />)(({ theme }) => ({
-  backgroundColor: '#919191',
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
 }));
 const AccordionSummaryStyled = styled((props) => <AccordionSummary {...props} />)(({ theme }) => ({
-  backgroundColor: '#919191',
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+  },
 }));
+
 const AccordionDetailsStyled = styled((props) => <AccordionDetails {...props} />)(({ theme }) => ({
-  backgroundColor: '#ccc',
+  padding: theme.spacing(2),
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
 }));
+
+
 
 export { AccordionStyled, AccordionSummaryStyled, AccordionDetailsStyled };
