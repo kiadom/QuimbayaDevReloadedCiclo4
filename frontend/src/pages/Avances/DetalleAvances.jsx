26ec26929086c770e9053283332cbfbdc2f02c13
Rocio Pacheco <pacheco.rocio@gmail.com>
2021-12-10 17:19:32 -0500

    se separa en detalle avances los valores a modificar

--- frontend/src/pages/Avances/DetalleAvances.jsx
+++ frontend/src/pages/Avances/DetalleAvances.jsx
@@ -4,22 +4,23 @@ import { useMutation, useQuery } from '@apollo/client';
 import useFormData from "../../hooks/useFormData";
 
 import { GET_AVANCES2 } from "../../graphql/avances/queries";
-import { EDITAR_AVANCE } from "../../graphql/avances/mutations";
+import { EDITAR_AVANCE } from "../../graphql/avances/mutations"
 
 
 /* FUNCION PRINCIPAL QUE SE EJECUTA, DESDE ACA SE LLAMAN LAS DEMAS FUNCIONES Y SE DEFINEN LOS ESTADOS */
 function DetalleAvances () {
     
-    const { form, formData, updateFormData } = useFormData();
+    const { form, formData, updateFormData } = useFormData(null);
     const { _id } = useParams();
 
    /*PLANTILLA PARA HACER LA PETICION GET DE AVANCES. EL RETORNO SE ALMACENA EN data */
-    const { data: queryData, loading } = useQuery(GET_AVANCES2, {
+    const { data: queryData, loading: queryLoading } = useQuery(GET_AVANCES2, {
         variables:{ _id },
     });
 
        
-    const [editarAvance, { data: mutationData }] = useMutation(EDITAR_AVANCE);
+    const [editarAvance, { data: mutationData, loading: mutationLoading }] 
+    = useMutation(EDITAR_AVANCE);
 
     const submitForm = (e) => {
         e.preventDefault();
@@ -29,7 +30,7 @@ function DetalleAvances () {
     };
 
      
-    if(!loading){
+    if(!queryLoading){
         return (
             <div className = "body-text">
                 <table>
@@ -44,11 +45,13 @@ function DetalleAvances () {
                 
                 <h1>ID del Avance Seleccionado: { _id }</h1> 
                 
-                <h1>Ingrese los nuevos datos del Avance</h1>
+                <h1>Estos son los datos que No Cambian del Avance</h1>
                 {/*
                 <form onSubmit = { submitForm } onChange = { updateFormData } ref = { form }>
-                */}   
+                */}
+
                 <table>
+                        
                         <tr>
                             <td>
                                 <p>Proyecto: </p>
@@ -85,44 +88,45 @@ function DetalleAvances () {
                             </td>
                         </tr>
                 </table>
+                        
+
+                <h1>Estos son los datos a modificar</h1>
 
                 <form onSubmit = { submitForm } onChange = { updateFormData } ref = { form }>
-                <table>
-                    <tr>
-                        <td>
-                            <p>Descripcion: </p>
-                        </td>
-                        <td>
-                            <input 
-                                name = 'descripcion' 
-                                defaultValue = { queryData.DetalleAvances.descripcion } 
-                                type = "text" 
-                            />
-                        </td>
-                    </tr>
+                    <table>
+                        <tr>
+                            <td>
+                                <p>Descripcion: </p>
+                            </td>
+                            <td>
+                                <input 
+                                    name = 'descripcion' 
+                                    defaultValue = { queryData.DetalleAvances.descripcion } 
+                                    type = "text" 
+                                />
+                            </td>
+                        </tr>
 
-                    <tr>
-                        <td>
-                            <p>Observaciones Lider: </p>
-                        </td>
-                        <td><input 
-                                name = 'observacionesLider' 
-                                defaultValue ={ queryData.DetalleAvances.observacionesLider } 
-                                type = "text" 
-                            />
-                        </td>
-                    </tr>
+                        <tr>
+                            <td>
+                                <p>Observaciones Lider: </p>
+                            </td>
+                            <td><input 
+                                    name = 'observacionesLider' 
+                                    defaultValue ={ queryData.DetalleAvances.observacionesLider } 
+                                    type = "text" 
+                                />
+                            </td>
+                        </tr>
 
-
-                    <tr>
-                        <td>
-                            <input 
-                                type = "submit" 
-                                value = "Guardar cambios" 
-                            />
-                        </td>
-                    </tr>
-                    
+                        <tr>
+                            <td>
+                                <input 
+                                    type = "submit" 
+                                    value = "Guardar cambios" 
+                                />
+                            </td>
+                        </tr>
                     </table>
                 </form>
             </div>
@@ -135,10 +139,6 @@ function DetalleAvances () {
             <h1>Cargando</h1>
         </div>
     );
-    
-    /* EN ESTE RETURN VA EL BOTON QUE PERMITE CAMBIAR DE INTERFAZ. 
-    AL DAR CLIC SOBRE ESTE, CAMBIA EL ESTADO DE mostrarTabla, LLAMANDO ASI AL FORMULARIO*/
-    
 };
 
 
