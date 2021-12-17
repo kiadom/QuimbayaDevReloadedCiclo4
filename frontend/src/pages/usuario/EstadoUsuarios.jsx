04219b33e4ec9f79ced9abf18008e21ede4e994b
kiadom <kiadom777@gmail.com>
2021-12-17 00:14:45 -0500

    css usuarios

--- frontend/src/pages/usuario/EstadoUsuarios.jsx
+++ frontend/src/pages/usuario/EstadoUsuarios.jsx
@@ -6,10 +6,11 @@ import { GET_USUARIO } from '../../graphql/usuarios/querys';
 import { EDITAR_USUARIO } from "../../graphql/usuarios/mutation"
 import { toast } from 'react-toastify';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import {faArrowLeft, faUsers} from "@fortawesome/free-solid-svg-icons";
 import DropDown from '../../components/DropDown';
 import { Enum_Rol, Enum_EstadoUsuario } from '../../utils/enums';
 import ButtonLoading from '../../components/ButtonLoading';
+import { Sidebar } from '../../components/Sidebar';
 
 const EstadoUsuarios = () => {
 
@@ -61,43 +62,49 @@ const EstadoUsuarios = () => {
 
     return (
         <div className='body-text'>
-            <Link to='/GestionUsuarios'>
-                <FontAwesomeIcon icon={ faArrowLeft } size="1x" color='#FFFFFF' className='cursor-pointer'/>
-            </Link>
-            <h1 className='rp_titulo'>Editar Estado Usuario</h1>
-            <br />
-            <form
-                onSubmit={submitForm}
-                onChange={updateFormData}
-                ref={form}
-                className='flex flex-col items-center justify-center'
-            >
-                <span>Id: {queryData.Usuario._id}</span>
-                <br/>
-                <span>Identificación: {queryData.Usuario.identificacion}</span>
-                <br/>
-                <span>Nombres: {queryData.Usuario.nombre}</span>
-                <br/>
-                <span>Apellidos: {queryData.Usuario.apellido}</span>
-                <br/>
-                <span>Correo Electronico: {queryData.Usuario.correo}</span>
-                <br/>
-                <span>Rol del usuario: {Enum_Rol[queryData.Usuario.rol]}</span>
-                <br/>
-                <DropDown
-                    label='Estado de la persona:'
-                    name='estado'
-                    defaultValue={queryData.Usuario.estado}
-                    required={true}
-                    options={Enum_EstadoUsuario}
-                />
-                <br/>
-                <ButtonLoading
-                disabled={Object.keys(formData).length === 0}
-                loading={mutationLoading}
-                text='Confirmar'
-                />
-            </form>
+            <Sidebar icono={faUsers} titulo='EDITAR ESTADO USUARIO'/>
+            <div className='contenedor-body'>
+                {/* <Link to='/GestionUsuarios'>
+                    <FontAwesomeIcon icon={ faArrowLeft } size="1x" color='#FFFFFF' className='cursor-pointer'/>
+                </Link> */}
+                <h1 className='rp_subtitulo'>Editar Estado Usuario</h1>
+                <br />
+                <div className='editarusuario'>
+                    <form
+                        onSubmit={submitForm}
+                        onChange={updateFormData}
+                        ref={form}
+                        
+                    >
+                        <span>Id: {queryData.Usuario._id}</span>
+                        <br/>
+                        <span>Identificación: {queryData.Usuario.identificacion}</span>
+                        <br/>
+                        <span>Nombres: {queryData.Usuario.nombre}</span>
+                        <br/>
+                        <span>Apellidos: {queryData.Usuario.apellido}</span>
+                        <br/>
+                        <span>Correo Electronico: {queryData.Usuario.correo}</span>
+                        <br/>
+                        <span>Rol del usuario: {Enum_Rol[queryData.Usuario.rol]}</span>
+                        <br/>
+                        <br/>
+                        <DropDown
+                            label='Estado de la persona:'
+                            name='estado'
+                            defaultValue={queryData.Usuario.estado}
+                            required={true}
+                            options={Enum_EstadoUsuario}
+                        />
+                        <br/>
+                        <ButtonLoading
+                        disabled={Object.keys(formData).length === 0}
+                        loading={mutationLoading}
+                        text='Confirmar'
+                        />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
